83773daabdad5007bc371074299639ada2ec247a
AlbertoIT <alberto.orso@gmail.com>
2018-05-03 08:39:33 +0300

    Update 19_paint.js

--- image/19_paint.js
+++ image/19_paint.js
@@ -164,6 +164,44 @@ function draw(pos, state, dispatch) {
   drawPixel(pos, state);
   return drawPixel;
 }
+
+function draw(pos, state, dispatch) {
+  function connect(newPos, state) {
+    let line = drawLine(pos, newPos, state.color);
+    pos = newPos;
+    dispatch({picture: state.picture.draw(line)});
+  }
+  connect(pos, state);
+  return connect;
+}
+
+function drawLine(from, to, color) {
+  let points = [];
+  if (Math.abs(from.x - to.x) > Math.abs(from.y - to.y)) {
+    if (from.x > to.x) [from, to] = [to, from];
+    let slope = (to.y - from.y) / (to.x - from.x);
+    for (let {x, y} = from; x <= to.x; x++) {
+      points.push({x, y: Math.round(y), color});
+      y += slope;
+    }
+  } else {
+    if (from.y > to.y) [from, to] = [to, from];
+    let slope = (to.x - from.x) / (to.y - from.y);
+    for (let {x, y} = from; y <= to.y; y++) {
+      points.push({x: Math.round(x), y, color});
+      x += slope;
+    }
+  }
+  return points;
+}
+
+function line(pos, state, dispatch) {
+  return end => {
+    let line = drawLine(pos, end, state.color);
+    dispatch({picture: state.picture.draw(line)});
+  };
+}
+
 function circle(pos, state, dispatch) {
   function drawCircle(to) {
     let radius = Math.sqrt(Math.pow(to.x - pos.x, 2) +
@@ -340,7 +378,7 @@ var startState = {
   doneAt: 0
 };
 
-var baseTools = {draw, fill, rectangle, circle, pick};
+var baseTools = {draw, fill, line, rectangle, circle, pick};
 
 var baseControls = [
   ToolSelect, ColorSelect, SaveButton, LoadButton, UndoButton
