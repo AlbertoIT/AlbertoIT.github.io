72e906ad3d13a54f393191c8a692ec53aa014108
AlbertoIT <alberto.orso@gmail.com>
2018-05-03 12:46:36 +0300

    Update 19_paint.js

--- image/19_paint.js
+++ image/19_paint.js
@@ -380,7 +380,7 @@ constructor(_, {dispatch}) {
 }
 
 function packPixels(dispatch) {
-    var canvas = this.dom;
+    var canvas = elt("canvas", {width, height});    
     var ctx = canvas.getContext('2d');    
     // get bitmap
     var idata = ctx.getImageData(0, 0, canvas.width, canvas.height),            // area to analyze
